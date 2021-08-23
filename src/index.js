203bb41bee9f2478904214276ce2cfd39b414937
dkreyman <dkreyman@gmail.com>
2021-08-23 13:06:23 -0600

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/stylesheet.css'
-import {BrowserRouter} from 'react-router-dom'
-import {createStore, applyMiddleware} from 'redux'
-import rootReducer from './redux/reducer'
-import {Provider} from 'react-redux'
-import App from './components/app'
-import thunk from 'redux-thunk'
-import {database} from './Database/config'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/stylesheet.css";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducer";
+import { Provider } from "react-redux";
+import App from "./components/app";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk));
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()      #for redux extension tool
 
-
-ReactDOM.render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
